76e0750fe498e190b85629d1f06afe70eb829406
Leandro Amaral <amaral_adm@hotmail.com>
2023-10-16 21:59:17 -0300

    Add IllegalArgumentException

--- src/main/java/br/com/amaral/consumer/Consumer.java
+++ src/main/java/br/com/amaral/consumer/Consumer.java
@@ -2,7 +2,9 @@ package br.com.amaral.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Component;
 
 import br.com.amaral.configuration.RabbitMQConfiguration;
@@ -18,10 +20,22 @@ public class Consumer {
 		System.out.println("====== End of Message ======");
 	}
 
+	@RetryableTopic(attempts = "5")
 	@KafkaListener(topics = "${kafka.model.name.topic}", groupId = "modelGroup")
 	public void consumeMessageByKafka(String message) {
+		if (message.contains("DLT")){
+			throw new IllegalArgumentException("DLT Test");
+		}
+		
 		System.out.println("====== Receiving Message ======");
 		System.out.println("====== " + message);
 		System.out.println("====== End of Message ======");
+	}
+	
+	@DltHandler
+	public void consumeMessageDLT(String message) {
+		System.out.println("====== Receiving DLT Message ======");
+		System.out.println("====== " + message);
+		System.out.println("====== End of DLT Message ======");
 	}
 }
